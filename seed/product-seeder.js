021858e8c2540cfd8b5162e2c9a753e9414590b7
Gloper98 <littleannygutlop_2014@outlook.es>
2018-05-13 17:46:32 -0500

    Updating database

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -11,13 +11,13 @@ var products = [
         price: 10
     }),
     new Product({
-        imagePath: 'http://www.pictorem.com/collection/900_Vancouver Canada vintage travel poster.jpg',
+        imagePath: 'https://i.pinimg.com/originals/43/03/ac/4303ace198992cc18af4a3c00dbb9f1b.jpg',
         title: 'The great USA',
         description: 'Awesome USA!!!',
         price: 10
     }),
     new Product({
-        imagePath: 'http://www.pictorem.com/collection/900_Vancouver Canada vintage travel poster.jpg',
+        imagePath: 'https://ih1.redbubble.net/image.366626914.7645/flat,800x800,070,f.u1.jpg',
         title: 'The great Britain',
         description: 'Awesome UK!!!',
         price: 10
@@ -33,13 +33,7 @@ var products = [
         title: 'The great Italy',
         description: 'Awesome Italy!!!',
         price: 10
-        }),
-    new Product({
-        imagePath: 'http://www.pictorem.com/collection/900_Vancouver Canada vintage travel poster.jpg',
-        title: 'The great France',
-        description: 'Awesome France!!!',
-        price: 10
-    })
+        })
 ];
 
 var done = 0;
